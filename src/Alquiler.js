81bb5406cee30618c1fcfe0eee313ab5cdbd8827
LuluNull <lucylemonyay@gmail.com>
2021-12-28 19:09:49 -0300

    Project start

---
+++ src/Alquiler.js
@@ -0,0 +1,18 @@
+import './Alquiler.css';
+
+function Alquiler() {
+  return (
+    <div className="App">
+      <header className="Alq-header">
+        <p>
+            ArgenProp
+        </p>
+      </header>
+      <content className="Alq-content">
+          holis
+      </content>
+    </div>
+  );
+}
+
+export default Alquiler;
